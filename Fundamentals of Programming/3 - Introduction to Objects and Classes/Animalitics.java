2107f154f7298147673956f509264904915317ba
John Angelo Basilio <111488301+monozi@users.noreply.github.com>
2024-10-19 00:25:37 +0800

    Create Animalitics.java

---
+++ Fundamentals of Programming/3 - Introduction to Objects and Classes/Animalitics.java
@@ -0,0 +1,32 @@
+class Animal {
+    // Attributes
+    String name;
+    String species;
+
+    // Constructor: used to create an object
+    public Animal(String name, String species) {
+        this.name = name;
+        this.species = species;
+    }
+
+    // Method: defines behavior of the object
+    public void displayInfo() {
+        System.out.println("This is a " + species + " named " + name);
+    }
+}
+
+// Main class to run the program
+public class Animalitics {
+    public static void main(String[] args) {
+        // Creating objects of the Animal class
+        Animal dog = new Animal("Buddy", "Dog");
+        Animal cat = new Animal("Whiskers", "Cat");
+
+        // Calling methods on the objects
+        dog.displayInfo();
+        cat.displayInfo();
+    }
+}
+
+
+
